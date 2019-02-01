480c54aea88beeb45e18ee25014b002711867bc7
Serj <Striker-Life@mail.ru>
2019-02-01 15:38:20 +0200

    fix bug with init driver

--- studyProject/src/main/java/base/conf/BaseTest.java
+++ studyProject/src/main/java/base/conf/BaseTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-    protected WebDriver driver = Driver.getWebDriver();
+    protected WebDriver driver;
 
     @BeforeSuite
     protected void initDriver () {
-        Driver.getWebDriver();
+        driver = Driver.getWebDriver();
     }
 
     @AfterSuite
